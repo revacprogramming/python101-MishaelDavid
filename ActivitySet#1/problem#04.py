b3f2507c6098c1305782087b152c5b5e5cfd7317
Mishael David <97089344+MishaelDavid@users.noreply.github.com>
2022-05-13 04:14:10 +0000

    .

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,13 @@
-# Conditional Execution
-
-hrs = input("Enter hours? ")
+hrs = input("Enter Hours:")
+h = float(hrs)
+r=float(input("Enter the rate:"))
+if h<=40:
+    tot = h*r
+    print( tot)
+else:
+    i = h - 40
+    t1=40 * r
+    t2=i*1.5*r
+    t=t1+t2
+    print(t)
+  
