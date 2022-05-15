3f52b997ceaf4d2ed473940dfdbbbf19eae15afd
Mishael David <97089344+MishaelDavid@users.noreply.github.com>
2022-05-15 18:01:07 +0000

    .

--- ActivitySet#1/problem#03.py
+++ ActivitySet#1/problem#03.py
@@ -1,12 +1,4 @@
-hrs = input("Enter Hours:")
-h = float(hrs)
-r=float(input("Enter the rate:"))
-if h<=40:
-    tot = h*r
-    print("Pay:", tot)
-else:
-    i = h - 40
-    t1=40 * r
-    t2=i*1.5*r
-    t=t1+t2
-    print("Pay:", t)
+h=float(input("Enter Hours = "))
+r=float(input("Enter Rate = "))
+pay=h*r
+print("Pay:",pay)
