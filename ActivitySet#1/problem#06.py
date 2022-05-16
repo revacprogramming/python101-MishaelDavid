b32a10012e4f1d1cf8d82f1de3a1b12eee5befdc
Mishael David <97089344+MishaelDavid@users.noreply.github.com>
2022-05-16 18:48:53 +0000

    .

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,14 +1,1 @@
-num = 0
-tot = 0.0
-while True :
-  text = input('Enter a number:')
-  if text == 'done' :
-    break
-  try:
-    atext = float(text)
-  except:
-    print('Invalid input')
-    continue
-    num = num + 1
-    tot = tot + atext
-print(tot,num,tot/num)
+num
